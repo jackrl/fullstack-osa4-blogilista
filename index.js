42f207ee8f1cfef883fc0f09584b088ef8c1353f
jackrl <jacinto.ramirez.lahti@cs.helsinki.fi>
2018-02-17 17:06:12 +0200

    Tehtävät 4.1-4.2

--- index.js
+++ index.js
@@ -4,42 +4,22 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-module.exports = Blog
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
